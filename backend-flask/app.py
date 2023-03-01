7ebfb4744432f66159798582dc1568f8df7e3fb8
Djaballah Mohammed Djedid <djaballah1994@live.com>
2023-03-01 07:09:06 +0000

    implement rollbar logs

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -31,6 +31,12 @@ import watchtower
 import logging
 from time import strftime
 
+# Rollbar
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 
 # Honeycomb
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -54,8 +60,8 @@ LOGGER.addHandler(console_handler)
 LOGGER.addHandler(cw_handler)
 LOGGER.info("Starting backend-flask")
 
-
 app = Flask(__name__)
+
 
 # Initialize automatic instrumentation with Flask
 # Honeycomb
@@ -64,6 +70,25 @@ RequestsInstrumentor().instrument()
 
 # X-ray
 XRayMiddleware(app, xray_recorder)
+
+# Rollbar initialization
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
 
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
@@ -180,6 +205,11 @@ def data_activities_reply(activity_uuid):
 @app.route("/healthy", methods=['GET'])
 def health_check():
   return {}, 200
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World! from Djaballah', 'warning')
+    return "Hello World!"
 
 if __name__ == "__main__":
   app.run(debug=True)
